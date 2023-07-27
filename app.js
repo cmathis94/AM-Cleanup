7824db8d71529c10280090b62af9642b3d204ada
cmathis94 <collin.mathis94@gmail.com>
2023-07-27 15:00:13 -0500

    AM Cleanup boilerplate

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/assets'));
 app.use(express.urlencoded({ extended: true }));
-// app.use(secure);
+app.use(secure);
 
 app.get('/', (req, res) => {
   res.render('home');
